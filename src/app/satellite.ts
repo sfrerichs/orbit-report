a29d6a70fb94d1186d334803e6714e88c8da6a93
SarahF <sarahfrerichs@gmail.com>
2019-10-31 00:39:03 -0500

    first submission: highlight and search need work

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,11 +4,38 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    // warning: boolean;
+
     constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
+        // this.warning = false;
     }
+
+    shouldShowWarning() :boolean {
+        return true;
+
+        // this.warning = true;
+        // return this.warning;
+        
+        // return type.toLowerCase === 'space debris';
+
+        // if (type.toLowerCase === "space debris") {
+        //     return true;
+        // } else {
+        //     return false;
+        // }  
+
+        // for (i=0;i<satellites.length;i++) {
+        //     if (satellite[i].type.toLowerCase === "space debris") {
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }   
+        // }
+    }
+    
 }
